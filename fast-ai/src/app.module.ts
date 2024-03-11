54f4e398471fa94a4ba2b0815d161082cf2b2eb6
SiAvMaAnR <siavmaanr@gmail.com>
2024-03-11 23:36:55 +0300

    refactor: rename modules

--- fast-ai/src/app.module.ts
+++ fast-ai/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import configuration from './config/configuration';
-import { AuthModule } from './components/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
--- fast-ai/src/modules/auth/auth.guard.ts
+++ fast-ai/src/modules/auth/auth.guard.ts
@@ -26,6 +26,7 @@ export class AuthGuard implements CanActivate {
     if (!token) {
       throw new UnauthorizedException();
     }
+
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: this.authConfig.accessToken.secret,
